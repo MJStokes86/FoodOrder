f4b6f77bdb3d88e863520634be8993239139c0ec
mjstokes86 <mjstokes1986@att.net>
2020-09-03 01:34:46 -0400

    View Order and Sign Out checkpoint 1:34am

---
+++ app/src/main/java/com/example/foodorderapp/OrderStatus.java
@@ -0,0 +1,98 @@
+package com.example.foodorderapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.foodorderapp.Common.Common;
+import com.example.foodorderapp.Interface.ItemClickListener;
+import com.example.foodorderapp.Model.Request;
+import com.example.foodorderapp.ViewHolder.OrderViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class OrderStatus extends AppCompatActivity {
+
+    public RecyclerView recyclerView;
+    public RecyclerView.LayoutManager layoutManager;
+
+    FirebaseRecyclerAdapter<Request, OrderViewHolder> adapter;
+
+    FirebaseDatabase database;
+    DatabaseReference requests;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_status);
+
+        //Firebase
+        database = FirebaseDatabase.getInstance();
+        requests = database.getReference("Requests");
+
+        recyclerView = (RecyclerView) findViewById(R.id.listOrders);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        loadOrders(Common.currentUser.getPhone());
+
+    }
+
+    private void loadOrders(String phone) {
+
+        FirebaseRecyclerOptions<Request> options = new FirebaseRecyclerOptions.Builder<Request>()
+                .setQuery(requests.orderByChild("phone").equalTo(phone), Request.class)
+                .build();
+
+            adapter = new FirebaseRecyclerAdapter<Request, OrderViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull OrderViewHolder holder, int position, @NonNull Request model) {
+                holder.txtOrderId.setText(adapter.getRef(position).getKey());
+                holder.txtOrderStatus.setText(convertCodeToStatus(model.getStatus()));
+                holder.txtOrderAddress.setText(model.getAddress());
+                holder.txtOrderPhone.setText(model.getPhone());
+
+                holder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public OrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View itemView = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.order_layout, parent, false);
+                return new OrderViewHolder(itemView);
+            }
+        };
+
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private String convertCodeToStatus(String status) {
+        if ("0".equals(status))
+            return "Placed";
+        else if ("1".equals(status))
+            return "On my way";
+        else
+            return "Shipped";
+    }
+
+    }
+
+
+
